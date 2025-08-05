3490f220110c7e013c7fbdf5a43544b4dba9f537
TiangYuan0327 <vincenttpe2255@gmail.com>
2025-08-05 14:44:36 +0800

    feat: 新增錯誤訊息處理

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import MapleStory from '../views/MapleStory.vue'
 import ProjectSekai from '../views/ProjectSekai.vue'
@@ -16,7 +16,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory('/GameExperience/'),
   routes,
   scrollBehavior(to) {
     if (to.hash) {
